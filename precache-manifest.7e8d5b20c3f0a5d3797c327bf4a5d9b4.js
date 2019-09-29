ab70de0ba4967daa85cbf0c1a053b3dd33467a4b
Nick <nick@nicknedev.com>
2019-09-29 14:34:16 -0700

    Updates

---
+++ precache-manifest.7e8d5b20c3f0a5d3797c327bf4a5d9b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f9c8fa21cd21e60c230d6b55b1ac20",
+    "url": "/assemble-fed-screen/index.html"
+  },
+  {
+    "revision": "6bb329889cd753c884c6",
+    "url": "/assemble-fed-screen/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c46638847cda2ee2505a",
+    "url": "/assemble-fed-screen/static/js/2.7bd2a786.chunk.js"
+  },
+  {
+    "revision": "6bb329889cd753c884c6",
+    "url": "/assemble-fed-screen/static/js/main.5e3e365b.chunk.js"
+  },
+  {
+    "revision": "f4ec2831ac6fd9059202",
+    "url": "/assemble-fed-screen/static/js/runtime-main.94036926.js"
+  }
+]);
